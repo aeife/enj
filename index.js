382022e8d0810ff8cbc13b1a2b27fb9af926a072
André Eife <andre.eife@gmail.com>
2016-02-19 23:21:30 +0100

    if developer key is missing, prompt and persist

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
 import evernoteClient from './src/evernoteClient';
 import program from 'commander';
 import config from './config.json';
+import inquirer from 'inquirer';
+import fs from 'fs';
 
 if (!config.developerKey) {
   console.log('Please fill in your developer token');
   console.log('To get a developer token, visit https://sandbox.evernote.com/api/DeveloperToken.action');
-  process.exit(1);
-}
-
-program
-  .version('0.0.1');
-program.parse(process.argv);
-
-if (program.args.length) {
-  evernoteClient.init(config, () => {
-    evernoteClient.addText(program.args.join(' '));
+  inquirer.prompt([{
+    type: 'input',
+    name: 'key',
+    message: '>'
+  }], function(answer) {
+    config.developerKey = answer.key;
+    fs.writeFile('./config.json', JSON.stringify(config, null, 2), err => {
+      if(err) {
+        console.log('error while saving settings');
+      } else {
+        console.log('setting saved');
+      }
+    });
   });
+} else {
+  program
+    .version('0.0.1');
+  program.parse(process.argv);
+
+  if (program.args.length) {
+    evernoteClient.init(config, () => {
+      evernoteClient.addText(program.args.join(' '));
+    });
+  }
 }
