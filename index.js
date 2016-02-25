f39ef3907b5df2f5d0a1330e7e6af9083a16c83a
André Eife <andre.eife@gmail.com>
2016-02-26 00:00:32 +0100

    separated prompts

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import evernoteClient from './src/evernoteClient';
 import config from './src/config.js';
+import prompts from './src/prompts.js';
 import program from 'commander';
 import inquirer from 'inquirer';
 import fs from 'fs';
@@ -11,30 +12,9 @@ console.log = () => {};
 winston.level = 'debug';
 
 if (!config.get().developerKey) {
-  console.info(`
-    Please fill in your developer token');
-    To get a developer token, visit https://sandbox.evernote.com/api/DeveloperToken.action
-  `);
-  inquirer.prompt([{
-    type: 'input',
-    name: 'key',
-    message: '>'
-  }], answer => {
-    config.set('developerKey', answer.key);
-  });
+  prompts.requestDeveloperToken(() => {});
 } else if (!config.get().notebook) {
-  evernoteClient.init(() => {
-    evernoteClient.getNotebooks(notebooks => {
-      inquirer.prompt([{
-        type: "list",
-        name: "notebook",
-        message: "Choose a notebook for your journal",
-        choices: notebooks.map(notebook => notebook.name)
-      }], answer => {
-        config.set('notebook', notebooks.find(notebook => notebook.name === answer.notebook).guid);
-      });
-    })
-  });
+  prompts.requestJournalNotebook(() => {});
 } else {
   program
     .version('0.0.1');
@@ -43,32 +23,12 @@ if (!config.get().developerKey) {
   if (program.args.length) {
     saveTextInEvernote(program.args.join(' '));
   } else {
-    console.info(`
-      Welcome to ENJ, the evernote journal in your terminal.
-      This is the multi line mode.
-      Type 'q' and press enter to submit your text
-    `);
-    multiLineText();
+    prompts.multiLineEntry(saveTextInEvernote);
   }
 }
 
 function saveTextInEvernote (text) {
   evernoteClient.init(() => {
     evernoteClient.addText(text);
-  });
-}
-
-function multiLineText(text = '') {
-  inquirer.prompt([{
-    type: "input",
-    name: "text",
-    message: ">"
-  }], answer => {
-      if (answer.text === "q") {
-        saveTextInEvernote(text);
-      } else {
-          text += answer.text + "\n";
-          multiLineText(text);
-      }
   });
 }
---
+++ src/prompts.js
@@ -0,0 +1,65 @@
+import config from './config.js';
+import evernoteClient from './evernoteClient.js';
+import winston from 'winston';
+import inquirer from 'inquirer';
+
+export default {
+  requestDeveloperToken,
+  requestJournalNotebook,
+  multiLineEntry
+};
+
+function requestDeveloperToken (cb) {
+  console.info(`
+    Please fill in your developer token');
+    To get a developer token, visit https://sandbox.evernote.com/api/DeveloperToken.action
+  `);
+  inquirer.prompt([{
+    type: 'input',
+    name: 'key',
+    message: '>'
+  }], answer => {
+    config.set('developerKey', answer.key);
+    cb();
+  });
+}
+
+function requestJournalNotebook (cb) {
+  evernoteClient.init(() => {
+    evernoteClient.getNotebooks(notebooks => {
+      inquirer.prompt([{
+        type: "list",
+        name: "notebook",
+        message: "Choose a notebook for your journal",
+        choices: notebooks.map(notebook => notebook.name)
+      }], answer => {
+        config.set('notebook', notebooks.find(notebook => notebook.name === answer.notebook).guid);
+        cb();
+      });
+    })
+  });
+}
+
+function multiLineEntry (cb) {
+  console.info(`
+    Welcome to ENJ, the evernote journal in your terminal.
+    This is the multi line mode.
+    Type 'q' and press enter to submit your text
+  `);
+  multiLineText(cb);
+}
+
+function multiLineText (cb, text = '') {
+  inquirer.prompt([{
+    type: "input",
+    name: "text",
+    message: ">"
+  }], answer => {
+      if (answer.text === "q") {
+        cb(text);
+      } else {
+          text += answer.text + "\n";
+          multiLineText(cb, text);
+      }
+  });
+}
